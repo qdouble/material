89765198c70f8527257cb1328570e7f345402fb8
Anthony <qdouble@gmail.com>
2016-10-31 06:01:02 -0700

    add effects to imports

--- src/app/app.imports.ts
+++ src/app/app.imports.ts
@@ -7,12 +7,16 @@ import { RouterStoreModule } from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
 
 import { routes } from './app.routing';
+import { OfferEffects } from './effects/offer';
+import { PrizeEffects } from './effects/prize';
 import { TicketEffects } from './effects/ticket';
 import { UserEffects } from './effects/user';
 import { rootReducer } from './reducers';
 import { StoreDevToolsModule } from './features/store-devtools.module';
 
 export const APP_IMPORTS = [
+  EffectsModule.run(OfferEffects),
+  EffectsModule.run(PrizeEffects),
   EffectsModule.run(TicketEffects),
   EffectsModule.run(UserEffects),
   FormsModule,
