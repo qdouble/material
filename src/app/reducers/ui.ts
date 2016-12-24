17b6854ff03e2272e386e9c26c6040fec983ea61
Anthony <qdouble@gmail.com>
2016-12-24 10:36:22 -0800

    update ui version

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -17,7 +17,7 @@ export const initialState: UIState = {
   mobile: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.0.11'
+  version: '0.0.12'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
