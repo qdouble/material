b78d6e684e3d4450c1b8b2994ca0fa6f150bbf5e
Anthony <qdouble@gmail.com>
2016-12-25 16:12:06 -0800

    fix referrals table and add css table

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -17,7 +17,7 @@ export const initialState: UIState = {
   mobile: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.0.12'
+  version: '0.0.13'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
