4d587cf9e7fffd4ad91ced76e21a9e02ff31f8c0
Anthony <qdouble@gmail.com>
2016-12-29 00:54:12 -0800

    bump to verision 0.0.20

--- src/app/features/status/common/referrals-table/referral-details.ts
+++ src/app/features/status/common/referrals-table/referral-details.ts
@@ -10,17 +10,25 @@ import { Referral } from '../../../../models/referral';
     <div class="referral-details">Referral Summary</div>
     <div>Username: {{referral.username}}</div>
     <div>Email: {{referral.email}}</div>
-    <div>Current Level: {{referral.currentLevel}}</div>
-    <div>Previous Level: {{referral.payBeyondLevel}}</div>
-    <div>Credits: {{credits | number: '1.2-2'}}</div>
-    <div [ngClass]="{'primary': (referral.currentLevel - referral.payBeyondLevel) > 0 }">
-      <b>Unpaid Levels: {{referral.currentLevel - referral.payBeyondLevel}}</b>
-    </div>
-    <div>Eligible for new order: <b>{{referral.leveledUp ? 'true' : 'false'}}</b></div>
-    <div class="warn bold" *ngIf="referral.hold">
-      On Hold: {{referral.hold ? 'true' : 'false' }}<br>
-      {{referral.holdReason}}
-    </div>
+    <ng-container *ngIf="referral.currentSponsor">
+      <div *ngIf="startedFrom">Start at Level: {{startedFrom}}</div>
+      <div>Current Level: {{referral.currentLevel}}</div>
+      <div>Previous Level: {{referral.payBeyondLevel}}</div>
+      <div>Credits: {{credits | number: '1.2-2'}}</div>
+      <div [ngClass]="{'primary': (referral.currentLevel - referral.payBeyondLevel) > 0 }">
+        <b>Unpaid Levels: {{referral.currentLevel - referral.payBeyondLevel}}</b>
+      </div>
+      <div>Eligible for new order: <b>{{referral.leveledUp ? 'true' : 'false'}}</b></div>
+      <div class="warn bold" *ngIf="referral.hold">
+        On Hold: {{referral.hold ? 'true' : 'false' }}<br>
+        {{referral.holdReason}}
+      </div>
+    </ng-container>
+    <ng-container *ngIf="!referral.currentSponsor">
+      <div>Level At Transfer: {{referral.currentLevel}}</div>
+      <b>Unpaid Levels: 0</b>
+      <div><b>User was transferred by user request.</b></div>
+    </ng-container>
     <button type="button" md-button color="accent" (click)="dialogRef.close()">CLOSE</button>
   </ng-container>`, // tslint:disable-line
   styles: [`.referral-details { border-bottom: 1px solid #ffe1ad; border-top: 1px solid #ffe1ad;
@@ -28,6 +36,7 @@ import { Referral } from '../../../../models/referral';
 })
 export class ReferralDetailsDialog implements OnInit {
   credits = 0;
+  startedFrom = 0;
   referral: Referral;
 
   constructor(public dialogRef: MdDialogRef<ReferralDetailsDialog>) { }
@@ -38,6 +47,9 @@ export class ReferralDetailsDialog implements OnInit {
           this.credits += credit.creditValue;
         }
       });
+    }
+    if (this.referral.levels[0][0] !== 0) {
+      this.startedFrom = this.referral.levels[this.referral.levels.length - 1][0];
     }
   }
 }
--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -17,7 +17,7 @@ export const initialState: UIState = {
   mobile: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.0.19'
+  version: '0.0.20'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
