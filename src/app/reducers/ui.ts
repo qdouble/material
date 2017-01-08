ffd9b43c448c40b3ac879c02e03ef54f32e7cd7c
Anthony <qdouble@gmail.com>
2017-01-07 20:35:15 -0800

    edit levels explanation in FAQ

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -21,7 +21,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.0.34'
+  version: '0.0.35'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
