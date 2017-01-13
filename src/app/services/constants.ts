5a32c53288377c605ce480c6de0787d0e73c78bf
Anthony <qdouble@gmail.com>
2017-01-12 19:53:31 -0800

    use https when publishing

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -1,6 +1,6 @@
 declare const LOCAL_IP;
 export const MOBILE = (typeof window !== 'undefined') ? (window.screen.availWidth < 800) : true;
-export const API_BASE_URL: string = `http://${HOST}:8089`;
+export const API_BASE_URL: string = `${PUBLISH ? 'https' : 'http'}://${HOST}:8089`;
 export const API_ADMIN_URL: string = `${API_BASE_URL}/admin`;
 export const API_USER_URL: string = `${API_BASE_URL}/user`;
 export const log = ENV === 'development' ? console.log.bind(console) : function() { };
