ee26cd8b7b600026bdf70d9fa8b92d0dbc7f88f8
Anthony <qdouble@gmail.com>
2017-01-20 23:05:58 -0800

    add hide to unqualifed for unlocked

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -29,7 +29,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.0.68'
+  version: '0.0.69'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
