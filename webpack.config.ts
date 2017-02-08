9a0a87f0a9479a151d53a025ee7cc248e683a95c
Anthony <qdouble@gmail.com>
2017-02-08 02:07:52 -0800

    minimify service worker

--- webpack.config.ts
+++ webpack.config.ts
@@ -210,6 +210,7 @@ const commonConfig = function webpackConfig(): WebpackConfig {
           maximumFileSizeToCacheInBytes: 4194304,
           navigateFallback: 'index.html',
           runtimeCaching: SW_RUNTIME_CACHING,
+          minify: true,
           // importScripts: ['sw-push.js']
         }
       ),
