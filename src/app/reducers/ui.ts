4716c5b2269a24ef7508797691d6f36ceb0b5ede
Anthony <qdouble@gmail.com>
2017-05-21 04:22:44 -0700

    add link for proof pic group

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -29,7 +29,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.1.7'
+  version: '0.1.8'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
