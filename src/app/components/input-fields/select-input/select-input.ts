f743daaabffae2e26925aa92901619b5614981a5
Anthony <qdouble@gmail.com>
2017-05-28 08:10:56 -0700

    fix centering on selected prize, make bank check green

--- src/app/components/input-fields/select-input/select-input.ts
+++ src/app/components/input-fields/select-input/select-input.ts
@@ -4,13 +4,14 @@ import { AbstractControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'select-input',
   templateUrl: './select-input.html',
-  styles: [` md-select { position: relative; top: -6px; } 
+  styles: [` md-select { position: relative; top: -6px; }
   label { margin-right: 7px; display: inline-table;}
-  md-select { overflow: hidden; } .form-group { display: flex; } 
+  md-select { overflow: hidden; } .form-group { display: flex; }
   .full-width { width: 100% }`]
 })
 
 export class SelectInput implements OnInit {
+  @Input() center: boolean;
   @Input() form: FormGroup;
   @Input() fullWidth: boolean;
   @Input() controlName: string;
