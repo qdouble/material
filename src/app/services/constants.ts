0b02e102bb7604285dbb7a058673f17daae77e1c
Anthony <qdouble@gmail.com>
2017-06-25 20:39:04 -0700

    fix package update and use user route instead of port

--- src/app/services/constants.ts
+++ src/app/services/constants.ts
@@ -2,7 +2,7 @@
 export const MOBILE = (typeof window !== 'undefined') ? (window.screen.availWidth < 800) : true;
 export const SERVICE_WORKER_SUPPORT = (typeof navigator !== 'undefined') ? 'serviceWorker' in navigator : false;
 export const PUSH_MANAGER_SUPPORT = (typeof window !== 'undefined') ? 'PushManager' in window : false;
-export const API_BASE_URL: string = `${PUBLISH ? 'https' : 'http'}://${HOST}:${PUBLISH ? '8443' : '8089'}`; // tslint:disable-line: max-line-length
+export const API_BASE_URL: string = `${PUBLISH ? 'https' : 'http'}://${HOST}${PUBLISH ? '' : ':8089'}`; // tslint:disable-line: max-line-length
 export const API_ADMIN_URL: string = `${API_BASE_URL}/admin`;
 export const API_USER_URL: string = `${API_BASE_URL}/user`;
 export const log = ENV === 'development' ? console.log.bind(console) : function() { };
