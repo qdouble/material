a6306e07ed237f4a0d4a55a843336e93bb7da719
Anthony <qdouble@gmail.com>
2017-08-17 19:14:49 -0700

    show credit total on step 1

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -34,7 +34,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.2.34'
+  version: '0.2.35'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
