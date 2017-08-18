038fe403b65b6265a3d34a6c7db3be27aa829f00
Anthony <qdouble@gmail.com>
2017-08-18 01:22:03 -0700

    fix max-width on offer agree checkboxes

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -34,7 +34,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.2.38'
+  version: '0.2.39'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
