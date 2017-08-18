caca846bef04783f598fabe896784bcc56340fd3
Anthony <qdouble@gmail.com>
2017-08-18 15:48:34 -0700

    update version to 0.2.41

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -34,7 +34,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.2.40'
+  version: '0.2.41'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
