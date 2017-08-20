41272a5b0004d6fab236e89e53d588028b005051
Anthony <qdouble@gmail.com>
2017-08-20 00:45:36 -0700

    state how many credit level 1 is worth

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -34,7 +34,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.2.49'
+  version: '0.2.50'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
