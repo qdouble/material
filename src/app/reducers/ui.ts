1ad6a0885aa5a3ab2d18c3f21d74a321307170ac
Anthony <qdouble@gmail.com>
2017-08-19 17:07:28 -0700

    reduce padding on title card

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -34,7 +34,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.2.44'
+  version: '0.2.45'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
