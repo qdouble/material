d7ea91ea451f0dc4fe4b9dde13f78a6bb6460e57
Anthony <qdouble@gmail.com>
2017-08-28 08:53:32 -0700

    undo page overview scroll

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -34,7 +34,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.2.68'
+  version: '0.2.69'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
