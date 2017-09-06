748d7aead7bb16bea76b8e3613f295514cdea1ca
Anthony <qdouble@gmail.com>
2017-09-06 14:36:35 -0700

    show new proof pic on completed order dialog

--- src/app/dialogs/completed-order.dialog.ts
+++ src/app/dialogs/completed-order.dialog.ts
@@ -11,12 +11,12 @@ import { Order } from '../models/order';
       <h5>Congratulations, Your Order Has Been Paid!</h5>
       <h6>Share a screenshot of your payment and inspire others!</h6>
       <p>Showing people that the program works will make it easier for you to get more signups and completions!</p>
-      <img src="assets/jpg/proof-pic-group-banner.jpg">
+      <img src="assets/jpg/fb-proof-banner-congrats.jpg">
       <a href="https://www.facebook.com/groups/irproofpics/?congrats=true" target="_blank"><button md-raised-button color="primary" class="white">Go to Proof Pic Group</button></a>
       <button md-button color="warn" (click)="dialogRef.close()">Close</button>
   </div>`,
   styles: [`.completed-order-dialog{ text-align: center; max-width: 500px; }
-  .completed-order-dialog img { max-height: 100px; max-width: 200px; } h5, h6 { margin: 0 }
+  .completed-order-dialog img { max-width: 100%; } h5, h6 { margin: 0 }
   .completed-order-dialog h5 { color: #638b35; margin-bottom: 10px; } h6 { font-weight: bold; }
   .main-text{ font-weight: bold; font-size: 15px }
   .completed-order-dialog button { margin-top: 5px; width: 100%; }
