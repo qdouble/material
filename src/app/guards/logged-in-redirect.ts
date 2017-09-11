b7d2f2830b801018d91e8ee576b9d2db044fa016
Anthony <qdouble@gmail.com>
2017-09-10 19:49:59 -0700

    redirect logged in users to the status page

--- src/app/guards/logged-in-redirect.ts
+++ src/app/guards/logged-in-redirect.ts
@@ -21,7 +21,7 @@ export class LoggedInRedirectGuard implements CanActivate {
     this.loggedIn$.filter(loggedIn => loggedIn !== null)
       .take(1).subscribe(loggedIn => {
         if (loggedIn) {
-          this.router.navigateByUrl('profile');
+          this.router.navigateByUrl('status');
         }
       });
     return !!this.isLoggedIn();
