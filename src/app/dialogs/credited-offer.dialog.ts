7c304843d3e7145e11e012a50c45a078d76e1da2
Anthony <qdouble@gmail.com>
2017-09-12 08:57:02 -0700

    update credited offer dialog formatting

--- src/app/dialogs/credited-offer.dialog.ts
+++ src/app/dialogs/credited-offer.dialog.ts
@@ -8,23 +8,21 @@ import { Offer } from '../models/offer';
   selector: 'os-credited-offer-dialog',
   template: `
   <div class="credited-offer-dialog">
-    <h5>Congratulations!</h5>
+    <h5><b>Congratulations!</b></h5>
     <h6 class="offer-details">
       You've Received {{offer.creditValue | number: '1.2-2'}} Credits for {{offer.displayName}}!
     </h6>
     <div><img class="os-offer-card-image" [src]="publish ? 'https://levelrewards.com/images/offer-images/' + offer.filename : (offer.directImageURL?.length > 1 ? offer.directImageURL : offer.imageURL)" [alt]="offer.displayName"></div>
     <div class="offer.reminder">
-        <span>
-          <p class="main-text" *ngIf="!offer.reminder">Be sure to fully try out offer for at least 90% of the trial period as quick/immediate cancellation or not trying the offer can cause your credit to be revoked.</p>
-          <pre class="main-text" *ngIf="offer.reminder">{{offer.reminder}}</pre>
-        </span>
-        <p *ngIf="needCredits" class="need-credits">You now have {{creditsTotal | number:'1.2-2'}} credits. You need {{needCredits | number: '1.2-2'}} more credits to make to to level {{nextLevel}} where you will be able to make {{'$' + nextLevel * 5}} per referral who reaches the same level!</p>
+        <p *ngIf="needCredits" class="need-credits">You now have {{creditsTotal | number:'1.2-2'}} credits. <b>You need {{needCredits | number: '1.2-2'}} more credits to make to to Level {{nextLevel}}</b> where you will be able to make <b>{{'$' + nextLevel * 5}} per referral</b> who reaches the same level!</p>
     </div>
     <button type="button" md-raised-button color="primary" class="white" (click)="dialogRef.close()">CONTINUE</button>
+    <p class="main-text" *ngIf="!offer.reminder">Be sure to fully try out offer for <u>at least 90% of the trial period</u> as quick/immediate cancellation or not trying the offer can cause your credit to be revoked.</p>
+    <pre class="main-text" *ngIf="offer.reminder">{{offer.reminder}}</pre>
   </div>`,
-  styles: [`.credited-offer-dialog{ text-align: center; }
+  styles: [`.credited-offer-dialog{ text-align: center; max-width: 460px; }
   .os-offer-card-image { max-height: 100px; max-width: 200px; } h5, h6 { margin: 0 } h5 { color: #638b35 }
-  .main-text{ font-weight: bold; font-size: 13px }
+  .main-text{ font-weight: bold; font-size: 13px; margin-top: 10px; }
   .credited-offer-dialog button { margin-top: 5px; width: 100%; }
   .mat-dialog-container {
     background: #fff !important;
