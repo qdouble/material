52688d2dadd8ea227235f8cf80800ad9050e12ab
Anthony <qdouble@gmail.com>
2017-09-27 22:32:02 -0700

    bump version to 0.4.7

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -34,7 +34,7 @@ export const initialState: UIState = {
   sendingContact: false,
   sideNavOpen: false,
   latestVersion: null,
-  version: '0.4.6'
+  version: '0.4.7'
 };
 
 export function uiReducer(state = initialState, action: Action): UIState {
