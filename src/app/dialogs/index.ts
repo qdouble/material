24e6e4a6f846ab84c6c9d22b8cee08851f9a306f
Anthony <qdouble@gmail.com>
2017-09-27 20:36:34 -0700

    remove getting started dialog

--- src/app/dialogs/index.ts
+++ src/app/dialogs/index.ts
@@ -1,12 +1,10 @@
 import { CompletedOrderDialog } from './completed-order.dialog';
 import { ConfirmDialog }  from './confirm.dialog';
 import { CreditedOfferDialog } from './credited-offer.dialog';
-import { GettingStartedDialog } from './getting-started.dialog';
 import { LevelBadgeDialog } from './level-badge.dialog';
 export const DIALOGS = [
   CompletedOrderDialog,
   ConfirmDialog,
   CreditedOfferDialog,
-  GettingStartedDialog,
   LevelBadgeDialog
 ];
