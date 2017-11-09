164f53570c13a69431d3435b77ff1cca7c57ded2
Anthony <qdouble@gmail.com>
2017-11-08 17:36:27 -0800

    update styling and test

--- src/app/effects/user.ts
+++ src/app/effects/user.ts
@@ -191,7 +191,7 @@ export class UserEffects {
           let showRefRand = Math.floor(Math.random() * 2);
           this.store.dispatch(this.offerActions.clearOffers());
           this.store.dispatch(go([res.payload.redirectTo,
-          { new: true, returning: true, showRefR: showRefRand }]));
+          { new: true, returning: true, showRefS: showRefRand }]));
           this.store.dispatch(this.userActions.testShowRefRandom(showRefRand));
           return;
         }
@@ -250,7 +250,7 @@ export class UserEffects {
         if (res.payload.redirectTo) {
           let showRefRand = Math.floor(Math.random() * 2);
           this.store.dispatch(go([res.payload.redirectTo,
-          { new: true, showRefR: showRefRand }]));
+          { new: true, showRefS: showRefRand }]));
           this.store.dispatch(this.userActions.testShowRefRandom(showRefRand));
         }
         if (!res.payload.success) {
--- src/app/features/offers/offers.ts
+++ src/app/features/offers/offers.ts
@@ -220,8 +220,8 @@ export class Offers implements AfterViewInit, OnDestroy, OnInit {
     (typeof document !== 'undefined' && document.getElementById('os-toolbar')) ? (document.getElementById('os-toolbar').scrollIntoView()) : {};  // tslint:disable-line
     this.route.params
       .subscribe(param => {
-        if (param['showRefR']) {
-          this.store.dispatch(this.userActions.testShowRefRandom(JSON.parse(param['showRefR'])));
+        if (param['showRefS']) {
+          this.store.dispatch(this.userActions.testShowRefRandom(JSON.parse(param['showRefS'])));
         }
         if (param['new']) {
           this.store.dispatch(this.userActions.newEqualTrue(true));
