b94cf13e17e3a548ba2af4d1523c756b62a97f47
Anthony <qdouble@gmail.com>
2018-05-29 02:32:05 -0500

    bump version to 0.6.6

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -48,7 +48,7 @@ export const initialState: State = {
   socialProofs: {},
   socialProofSettings: null,
   latestVersion: null,
-  version: '0.6.5'
+  version: '0.6.6'
 };
 
 export function uiReducer(state = initialState, action: UIActions): State {
