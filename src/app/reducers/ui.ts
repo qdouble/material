52053d12613365668312247633b23c2625fbd1d6
Anthony <qdouble@gmail.com>
2018-05-29 20:03:43 -0500

    fix forgot password issue

--- src/app/reducers/ui.ts
+++ src/app/reducers/ui.ts
@@ -48,7 +48,7 @@ export const initialState: State = {
   socialProofs: {},
   socialProofSettings: null,
   latestVersion: null,
-  version: '0.7.0'
+  version: '0.7.1'
 };
 
 export function uiReducer(state = initialState, action: UIActions): State {
