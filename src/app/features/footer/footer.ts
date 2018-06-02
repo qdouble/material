bc050e5aad7d273fb6ed8a9220777abb7b2cb1b9
Anthony <qdouble@gmail.com>
2018-06-02 15:57:14 -0500

    box-shadow-style-updates

--- src/app/features/footer/footer.ts
+++ src/app/features/footer/footer.ts
@@ -4,14 +4,27 @@ import { Component } from '@angular/core';
   selector: 'os-footer',
   template: `
   <mat-card-footer>
+    <div class="footer-content">
+    </div>
     <a [routerLink]="['/terms-and-conditions']">Terms and Conditions</a> |
     <a [routerLink]="['/privacy-policy']">Privacy Policy</a> |
     <a [routerLink]="['/crediting-guidelines']">Crediting Guidelines</a>
   </mat-card-footer>
   `,
   styles: [
-    `mat-card-footer {text-align: center}
-  a{font-size: 11px; cursor: pointer; color: #547a29}`
+    `
+      mat-card-footer {
+        text-align: center;
+      }
+      a {
+        font-size: 11px;
+        cursor: pointer;
+        color: #547a29;
+      }
+      .footer-content {
+        padding-top: 15px;
+      }
+    `
   ]
 })
 export class FooterComponent {}
