f3e4e462bb4994aa37d0fd9fa24430816e1be82e
Anthony <qdouble@gmail.com>
2018-06-05 10:01:41 -0500

    bump version to 0.9.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -417,7 +417,7 @@ export class AppComponent implements OnInit {
       .pipe(filter(s => s && s.length > 0))
       .subscribe(scripts => this.loadScripts(scripts));
 
-    if (this.version === '0.9.1') {
+    if (this.version === '0.9.2') {
       this.socialProofSettings$
         .pipe(filter(s => s !== undefined && s !== null))
         .subscribe(settings => {
